0140090b2629d8e8d8d93f62028a88bb563d7535
lorenzopugina <lorenzopugina10@gmail.com>
2025-02-25 16:52:14 -0300

    testes

--- teste.js
+++ teste.js
@@ -11,9 +11,8 @@ console.log(arredondar.toFixed(2));
 })();  // função que se auto executa, não requer nome mas pode ter
 
 
-// Objetos
-
-const pessoa = {
+// ----- criando um objeto
+const user = {
     idade: 21,
     nome: 'Lorenzo',
     forte: false,
@@ -22,8 +21,48 @@ const pessoa = {
     }
 };
 
-pessoa.altura = 1.81;
+user.altura = 1.81;
+user.dizerOi();
 
-console.log(pessoa);
+// ------------ Criando classes para serem instanciadas
 
-pessoa.dizerOi();
+class pessoas {
+    nome;
+    idade;
+    anoNascimento;
+
+    // constructor sempre é execuado quando uma pessoa é instanciada
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+        this.anoNascimento = 2025 - idade;
+    }
+
+    introduzirSe(){
+        console.log(`meu nome é ${this.nome} e tenho ${this.idade} anos`);
+    }
+
+}
+
+// criando objetos de classes (instancias)
+const joao = new pessoas('Joao', 37);
+joao.introduzirSe();
+
+
+function compararIdades(p1,p2){
+    if(p1.idade > p2.idade){
+        console.log(`${p1.nome} é mais velho que ${p2.nome}`);
+    }
+    else if(p2.idade > p1.idade){
+        console.log(`${p2.nome} é mais velho que ${p1.nome}`);
+    }
+    else{
+        console.log("Idades iguais!");
+    }
+}
+
+const jonatas = new pessoas('Jonatas', 31);
+const leonado = new pessoas('Leonardo', 20);
+
+compararIdades(jonatas, leonado);
+
