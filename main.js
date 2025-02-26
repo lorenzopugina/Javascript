8fb995a02c4cf810500fde21fe1866d44836a3e7
lorenzopugina <lorenzopugina10@gmail.com>
2025-02-26 16:06:18 -0300

    projeto api pokedex

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
- // importar as funçoes de outro arquivo como objeto
- import { gets, print } from './funcoes.js';
 
- const media = gets();
+function faz(){
+    console.log("Linkou");
 
- if(media < 5){
-    print("Reprovado");
- }
-
- else if(media < 7){
-    print("recuperação");
- }
- else{
-    print("Aprovado");
- }
- 
+}
